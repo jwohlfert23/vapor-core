43340fa877dbf4713b17c1452014e7dea6106227
Jack Wohlfert <jack.wohlfert@gmail.com>
2019-12-28 15:58:43 -0800

    Fix issue with NakedDomain middleware

--- src/Runtime/Http/Middleware/EnsureOnNakedDomain.php
+++ src/Runtime/Http/Middleware/EnsureOnNakedDomain.php
@@ -10,16 +10,18 @@ class EnsureOnNakedDomain
     /**
      * Handle the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  callable  $next
+     * @param \Illuminate\Http\Request $request
+     * @param callable $next
      * @return mixed
      */
     public function handle($request, $next)
     {
-        if (strpos($request->getHost(), 'www.') === 0) {
-            return new RedirectResponse(Str::replaceFirst(
-                'www.', '', $request->fullUrl()
-            ), 301);
+        if (strpos($request->getHost(), 'vapor-farm') !== false) {
+            if (strpos($request->getHost(), 'www.') === 0) {
+                return new RedirectResponse(Str::replaceFirst(
+                    'www.', '', $request->fullUrl()
+                ), 301);
+            }
         }
 
         return $next($request);
